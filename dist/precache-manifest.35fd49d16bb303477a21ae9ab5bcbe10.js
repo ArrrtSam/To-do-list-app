56c19d46a4741ecf0bfedfc244d6d76f1759d394
ArrrtSam <artemsamilenko19999@gmail.com>
2019-11-30 22:02:41 +0200

    fix

---
+++ dist/precache-manifest.35fd49d16bb303477a21ae9ab5bcbe10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f953f781e72f63381092",
+    "url": "/css/app.ef2f02e9.css"
+  },
+  {
+    "revision": "a3615bafcb94ff65f9c5",
+    "url": "/css/chunk-vendors.9ed95b7a.css"
+  },
+  {
+    "revision": "5b2a83aedb0a574ac44483105e5778fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f953f781e72f63381092",
+    "url": "/js/app.388fa587.js"
+  },
+  {
+    "revision": "a3615bafcb94ff65f9c5",
+    "url": "/js/chunk-vendors.e5eb5aa8.js"
+  },
+  {
+    "revision": "bc9f236f8ed43176935777754a23ac29",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
